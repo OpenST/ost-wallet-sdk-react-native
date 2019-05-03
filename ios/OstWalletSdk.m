17d0d5deffb9c9aadf2b39f3d2f38d71a2962be7
Akshay Raje <akshay.raje@gmail.com>
2019-05-03 15:53:42 +0530

    minor fix

--- ios/OstWalletSdk.m
+++ ios/OstWalletSdk.m
@@ -1,14 +1,12 @@
 #import "OstWalletSdk.h"
 #import <React/RCTLog.h>
 
-@interface RCT_EXTERN_REMAP_MODULE(OstSdk, OstWalletSdkS, NSObject)
-
-RCT_EXTERN_METHOD(initialize)
-
-@end
-
 @implementation OstWalletSdk
 
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
 RCT_EXPORT_MODULE();
 
 RCT_EXPORT_METHOD(initialize:(NSString *)url )
