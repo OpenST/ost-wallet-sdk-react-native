7b6e2ae79391a5d80023a18f1ecb8429554fa4ae
Ashutosh <ashutosh@ost.com>
2019-05-06 12:50:33 +0530

    poc

---
+++ ios/OstWalletRnBridge.h
@@ -0,0 +1,13 @@
+//
+//  OstWalletRnBridge1.h
+//  OstWalletRnSdk
+//
+//  Created by Ashutosh Lodhi on 06/05/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OstWalletRnBridge : NSObject
+@end
+
--- ios/OstWalletRnBridge.m
+++ ios/OstWalletRnBridge.m
@@ -1,11 +1,18 @@
 //
-//  OstWalletRnBridge.m
+//  OstWalletRnBridge1.m
 //  OstWalletRnSdk
 //
-//  Created by Ashutosh Lodhi on 05/05/19.
+//  Created by Ashutosh Lodhi on 06/05/19.
 //  Copyright © 2019 Facebook. All rights reserved.
 //
+
+#import "OstWalletRnBridge.h"
 #import <React/RCTBridgeModule.h>
+
+@implementation OstWalletRnBridge
+
+@end
+
 
 @interface RCT_EXTERN_MODULE(OstWalletRnSdk, NSObject)
 
