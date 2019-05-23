0fb207512e8a80a9e2b7dad7f91f5c86395025d2
Akshay Raje <akshay.raje@gmail.com>
2019-05-23 17:29:21 +0530

    rnsdk stable module

---
+++ android/src/main/java/com/ostwalletrnsdk/sdkIntracts/OstVerifyDataWrap.java
@@ -0,0 +1,29 @@
+package com.ostwalletrnsdk.sdkIntracts;
+import com.ost.walletsdk.workflows.interfaces.OstVerifyDataInterface;
+
+
+public class OstVerifyDataWrap extends BaseSdkInteract {
+
+    public  OstVerifyDataWrap(OstVerifyDataInterface ostVerifyDataInterface, String workflowCallbackId ){
+        super(ostVerifyDataInterface,workflowCallbackId);
+    }
+
+    public static OstVerifyDataWrap getInstance(String uuid ){
+        return (OstVerifyDataWrap) map.get( uuid );
+    }
+
+    void dataVerified( ) {
+        ((OstVerifyDataInterface)getSdkCallbackForAction()).dataVerified( );
+    }
+
+    public boolean messageReceived(String methodName ) {
+        if ( super.messageReceived(methodName, null) ) {
+            return true;
+        }
+
+        this.dataVerified( );
+        return true;
+    }
+
+
+}
