2685d00b5ad458e4f69daf45a63785e9480bf660
kevinfs510 <kevin@ost.com>
2020-03-12 15:34:22 +0100

    docs setup step #2

---
+++ docs/modules/ROOT/examples/ost-sdk-theme-config.js
@@ -0,0 +1,68 @@
+export default {
+
+  "nav_bar_logo_image": {
+    "asset_name": "header_icon"
+  },
+  
+  "c1": {
+    "size": 14,
+    "color": "#484848",
+    "system_font_weight": "bold",
+    "alignment": "left"
+  },
+  "b1":{
+    "color":"#ffffff",
+    "size":15
+  },
+    
+  "c2": {
+    "size": 12,
+    "color": "#6F6F6F",
+    "system_font_weight": "regular",
+    "alignment": "left"
+  },
+
+  "navigation_bar": {
+    "tint_color": "#61B2C9"
+  },
+
+  "navigation_bar_header": {
+    "tint_color": '#ffffff'
+  },
+
+  "icons": {
+    "close": {
+      "tint_color": "#ffffff"
+    },
+    "back":{
+      "tint_color": "#ffffff",
+      "source" : null
+    }
+  },
+
+  "cell_separator": {
+    "color": "#DBDBDB"
+  },
+
+  "link": {
+    "size": 14,
+    "color": "#007aff",
+    "system_font_weight": "medium",
+    "alignment": "left",
+  },
+
+  "status": {
+    "size": 14,
+    "color": "#0F9D58",
+    "system_font_weight": "regular",
+    "alignment": "left",
+  },
+
+  "form_field": {
+    "size":15,
+    "system_font_weight":"regular",
+    "color":"#484848",
+    "border_color": "#484848",
+    "alignment": "left"
+  }
+}
