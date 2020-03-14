d3e0405c1223e7951aa0e83fd7000a39e9cb7ef5
kevinfs510 <kevin@ost.com>
2020-03-14 15:08:28 +0100

    content updates

---
+++ docs/modules/ROOT/examples/initialize.js
@@ -0,0 +1,8 @@
+/**
+   * Initialize wallet sdk
+   * @param {String} endpoint - Ost Platform endpoint
+   * @param {function} Callback function with error and success status.
+   * @public
+   */
+  OstWalletSdk.initialize( endpoint,
+   (error, success) => {})
